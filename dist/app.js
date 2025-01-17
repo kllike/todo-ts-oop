442427ba1293ac1a5b2ad1ca824c7a47447deecc
Kullike Annuk <kullike.annuk@voco.ee>
2025-01-17 11:12:09 +0200

    Create new Todo from user data

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const inspector_1 = require("inspector");
+const body_parser_1 = require("body-parser");
 const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
+app.use((0, body_parser_1.json)());
 app.use('./todo', todos_1.default);
 app.use((error, req, res, next) => {
     res.json({ message: error.message });
